865bd2b81af62baca606d5d0da46741a237e9f39
bmdarshan <darshan.bmg@gmail.com>
2015-12-31 12:18:06 -0500

    r.js optimization

--- main.js
+++ main.js
@@ -2,16 +2,7 @@
 * @Author: bmdarshan
 * @Date:   2015-12-25 20:11:17
 * @Last Modified by:   bmdarshan
-* @Last Modified time: 2015-12-27 18:32:07
+* @Last Modified time: 2015-12-31 11:51:43
 */
-
-requirejs.config({
-    baseUrl: 'public',
-    paths: {
-        react:'lib/react/react',
-        reactBootstrap: 'lib/react-bootstrap/react-bootstrap',
-        reactDom:'lib/react-dom/react-dom'
-    },
-});
 
 requirejs(['app/pages/home']);
